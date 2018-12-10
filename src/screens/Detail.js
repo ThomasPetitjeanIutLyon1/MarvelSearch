a2c49bae0c08a8f54aa966ec229b29574ad7df16
ThomasP <trolldu25@live.fr>
2018-12-10 14:17:15 +0100

    Adding project structure

---
+++ src/screens/Detail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { datas :{
+            picUrl : "https://images-na.ssl-images-amazon.com/images/I/41XX8Bzd4vL._SX425_.jpg",
+        } };
+      }
+
+
+    render()
+}
+
+export default Detail;
